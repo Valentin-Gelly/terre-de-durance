10226ce6c72b6cb6d2f3baaba17b57535235ef52
valentin <vale84510@gmail.com>
2024-05-15 22:10:53 +0200

    beggining header routing need to be implements

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+import { HomePageComponent } from './home-page/home-page.component';
+import { HeaderComponent } from './header/header.component';
+import { HistoryComponent } from './history/history.component';
+
+export const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'header', component: HeaderComponent },
+    { path: 'history', component: HistoryComponent }
+
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-header',
