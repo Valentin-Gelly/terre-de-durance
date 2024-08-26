f6816762149675ac69ee8dd8fe3ca20437064608
valentin <vale84510@gmail.com>
2024-08-26 16:50:17 +0200

    t

---
+++ src/app/jus/jus.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jus',
+  standalone: true,
+  imports: [],
+  templateUrl: './jus.component.html',
+  styleUrl: './jus.component.css'
+})
+export class JusComponent {
+
+}
