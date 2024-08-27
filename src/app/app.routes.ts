4c8228f435b1ba5a6f09eead98a1b48128148c8e
ValentinGELLY <144439927+ValentinGELLY@users.noreply.github.com>
2024-08-27 10:03:50 +0200

    ajout savoir faire page, mise en forme de la page histoire à revoir avec jonathan

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,15 +5,17 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { HeaderComponent } from './header/header.component';
 import { HistoryComponent } from './history/history.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SavoirFaireComponent } from './savoir-faire/savoir-faire.component';
 import {FooterComponent} from "./footer/footer.component";
 import { NgxParallaxModule } from '@yoozly/ngx-parallax';
 export const routes: Routes = [
   { path: 'homePage', component: HomePageComponent },
   { path: '', component: HomePageComponent },
-    { path: 'header', component: HeaderComponent },
-    { path:'footer', component: FooterComponent },
-    { path: 'history', component: HistoryComponent },
-    { path: '**', component: PageNotFoundComponent }
+  { path: 'header', component: HeaderComponent },
+  { path:'footer', component: FooterComponent },
+  { path: 'savoir-faire', component: SavoirFaireComponent},
+  { path: 'history', component: HistoryComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
